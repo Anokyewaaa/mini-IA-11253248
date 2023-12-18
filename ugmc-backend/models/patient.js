3793a0f8ccb0310f03dc6337fbdb91255022558b
ANOKYEWAA AKONNOR <anokyewaaa467@gmail.com>
2023-12-18 09:40:56 +0000

    i created this folder earlier and i thought i had posted it in my repository it was an honest mistake

---
+++ ugmc-backend/models/patient.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+  patientID: String,
+  surname: String,
+  otherNames: String,
+  gender: String,
+  phoneNumber: String,
+  residentialAddress: String,
+  emergencyName: String,
+  emergencyContact: String,
+  relationshipWithPatient: String,
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
